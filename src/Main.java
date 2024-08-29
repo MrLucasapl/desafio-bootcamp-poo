528942cfd80f3179f8896a3ec9176ce4d314b067
MrLucasapl <lucas.apereira20@gmail.com>
2024-08-29 16:34:40 -0300

    finalizado

--- src/Main.java
+++ src/Main.java
@@ -25,28 +25,14 @@ public class Main {
         bootcamp.getConteudos().add(mentoria);
 
         Dev dev1 = new Dev();
-        dev1.setNome("Camila");
+        dev1.setNome("Lucas");
         dev1.inscreverBootcamp(bootcamp);
-        System.out.println("Conteúdos Inscritos Camila:" + dev1.getConteudosInscritos());
+        System.out.println("Conteúdos Inscritos Lucas:" + dev1.getConteudosInscritos());
         dev1.progredir();
         dev1.progredir();
         System.out.println("-");
-        System.out.println("Conteúdos Inscritos Camila:" + dev1.getConteudosInscritos());
-        System.out.println("Conteúdos Concluídos Camila:" + dev1.getConteudosConcluidos());
+        System.out.println("Conteúdos Inscritos Lucas:" + dev1.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Lucas:" + dev1.getConteudosConcluidos());
         System.out.println("XP:" + dev1.calcularTotalXp());
-
-        System.out.println("-------");
-
-        Dev dev2 = new Dev();
-        dev2.setNome("Joao");
-        dev2.inscreverBootcamp(bootcamp);
-        System.out.println("Conteúdos Inscritos João:" + dev2.getConteudosInscritos());
-        dev2.progredir();
-        dev2.progredir();
-        dev2.progredir();
-        System.out.println("-");
-        System.out.println("Conteúdos Inscritos João:" + dev2.getConteudosInscritos());
-        System.out.println("Conteúdos Concluidos João:" + dev2.getConteudosConcluidos());
-        System.out.println("XP:" + dev2.calcularTotalXp());
     }
 }
